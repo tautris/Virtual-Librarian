740ced2c32152824875814948d1334ec5f19225d
TDirmeikis <tdirmeikis@gmail.com>
2018-09-16 20:52:12 +0300

    Created Initial Book, BookCopy and User classes

--- Virtual Librarian/Virtual Librarian/Program.cs
+++ Virtual Librarian/Virtual Librarian/Program.cs
@@ -14,9 +14,16 @@ namespace Virtual_Librarian
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var book1 = new Book ("978-3-16-148410-0", "Red Riding Hood", "John", "Smith", DateTime.Today.AddYears(-1));
+            var bookIssue = new BookCopy(book1, DateTime.Today.AddMonths(-1));
+            var user1 = new User("Tautvydas", "Dirmeikis", "170000", User.Faculty.MIF);
+            user1.TakeBook(bookIssue);
+            user1.printTakenBooks();
+
+            //To launch windows form
+            //Application.EnableVisualStyles(); 
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
         }
     }
 }
