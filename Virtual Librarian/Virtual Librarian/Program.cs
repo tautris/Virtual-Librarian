13bf3f81df4f40b9be50c202f3d075335266b799
Tautrimas Gumbys <tautrimas.g2@gmail.com>
2018-09-22 22:01:56 +0300

    Remove unnecessary imports

--- Virtual Librarian/Virtual Librarian/Program.cs
+++ Virtual Librarian/Virtual Librarian/Program.cs
@@ -1,21 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Emgu.CV;
-using Emgu.CV.UI;
-using Emgu.CV.Structure;
-using System.Drawing;
-using System.Windows.Forms;
+
 
 namespace Virtual_Librarian
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
