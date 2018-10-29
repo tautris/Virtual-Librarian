5a09d160f112a3fb24a6c60813b3661dd9331ac3
Agota <agota.kazenaite@yahoo.com>
2018-10-29 21:33:43 +0200

    tests for taking and returning book

--- Virtual Librarian/VirtualLibrarianTests/UserAccountTests.cs
+++ Virtual Librarian/VirtualLibrarianTests/UserAccountTests.cs
@@ -44,6 +44,42 @@ namespace VirtualLibrarianTests
 
             Library.Instance.RemoveBook(book1);
         }
+        [TestMethod]
+        public void takeBook()
+        {
+            User user = new User(1, "Test", "Testy");
+            Book book1 = new Book("978-1-56619-909-4", "Crime and Punishment", "Feodor", "Dostoyevsky", DateTime.Now.AddYears(-30));
+            BookCopy bookCopy1 = new BookCopy(1, book1, DateTime.Now.AddMonths(-11));
+
+            var userBookCount = user.TakenBooks().Count;
+          
+            user.TakeBook(book1);
+
+            Assert.AreEqual(userBookCount + 1, user.TakenBooks().Count);
+           
+            Library.Instance.RemoveBookCopy(bookCopy1, bookCopy1.book);
+            Library.Instance.RemoveBook(book1);
+            user = null;
+        }
+
+        [TestMethod]
+        public void returnBook()
+        {
+            User user = new User(1, "Test", "Testy");
+            Book book1 = new Book("978-1-56619-909-4", "Crime and Punishment", "Feodor", "Dostoyevsky", DateTime.Now.AddYears(-30));
+            BookCopy bookCopy1 = new BookCopy(1, book1, DateTime.Now.AddMonths(-11));
+
+            var userBookCount = user.TakenBooks().Count;
+
+            user.TakeBook(book1);
+            user.ReturnBook(user.TakenBooks()[0]);
+
+            Assert.AreEqual(userBookCount , user.TakenBooks().Count);
+
+            Library.Instance.RemoveBookCopy(bookCopy1, bookCopy1.book);
+            Library.Instance.RemoveBook(book1);
+            user = null;
+        }
 
     }
 }
