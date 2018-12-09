e737469c315b82ed9bd5ebc1917287089d8a8d8c
Tautvydas Dirmeikis <tautvydas.dirmeikis@devbridge.com>
2018-12-09 19:19:23 +0200

    Simple hardcoded Search Bar implementation

--- virtual_librarian/lib/views/home_screen.dart
+++ virtual_librarian/lib/views/home_screen.dart
@@ -108,7 +108,7 @@ class _HomeScreenState extends State<HomeScreen> {
               new IconButton(
                 icon: new Icon(Icons.search),
                 onPressed: () {
-                  // TODO: implement
+                  showSearch(context: context, delegate: DataSearch());
                 },
               ),
             ],
@@ -127,3 +127,101 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+class DataSearch extends SearchDelegate<String> {
+  final books = [
+    "notes from underground",
+    "1984",
+    "aaaaa",
+    "Animal Farm",
+    "Etiquete",
+  ];
+
+  final recentBooks = [
+    "Animal Farm",
+    "Etiquete",
+  ];
+
+  @override 
+  ThemeData appBarTheme(BuildContext context) {
+    assert(context != null);
+    final ThemeData theme = Theme.of(context);
+    assert(theme != null);
+    return theme.copyWith(
+      primaryColor: Color(0x44FFEEFF),
+      primaryIconTheme: theme.primaryIconTheme.copyWith(color: Colors.white),
+      primaryColorBrightness: Brightness.dark,
+      primaryTextTheme: theme.textTheme,
+    );
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [IconButton(icon: Icon(Icons.clear), onPressed: (){
+      if (query.isEmpty) {
+        close(context, null);
+      } else {
+        query = "";
+      }
+    })];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: (){
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        child: Card(
+          color: Colors.red,
+          shape: StadiumBorder(),
+          child: Center(
+            child: Text(query),
+          ),
+        )
+      )
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty 
+    ? recentBooks 
+    : books.where((b) => b.toString().toLowerCase().contains(query.toLowerCase())).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        onTap: () {
+          showResults(context);
+        },
+        leading: Icon(Icons.book),
+        title: RichText(
+          text: TextSpan(
+            text: suggestionList[index].substring(0, query.length),
+            style: TextStyle(
+              color: Color(0xBB6BFDD9), fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                  text: suggestionList[index].substring(query.length),
+                  style: TextStyle(color: Colors.grey)
+                ),
+              ]
+            )
+          ),
+      ),
+      itemCount: suggestionList.length,
+    );
+  }}
