755f93d0b95fd611245ee4e0f87e88c209de35d9
Agota <agota.kazenaite@yahoo.com>
2018-12-17 23:03:09 +0200

    deleted surname from  domain.models.User

--- Virtual Librarian/VirtualLibrarian.Domain/Models/User.cs
+++ Virtual Librarian/VirtualLibrarian.Domain/Models/User.cs
@@ -6,7 +6,7 @@ namespace VirtualLibrarian.Domain.Models
     public class User : Base
     {
         public string Name { get; set; }
-        public string Surname { get; set; }
+        //public string Surname { get; set; }
         public FacultyEnum CurrentFaculty { get; set; }
         [ForeignKey("Admin")]
         public int? AdminId { get; set; }
