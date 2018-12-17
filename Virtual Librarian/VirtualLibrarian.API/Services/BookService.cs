5de01fb5a1429bce1cfc44dffe11a1fc67ea8781
Agota <agota.kazenaite@yahoo.com>
2018-12-17 21:27:04 +0200

    created book service for thread requirements

---
+++ Virtual Librarian/VirtualLibrarian.API/Services/BookService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using VirtualLibrarian.Domain;
+
+namespace VirtualLibrarian.API.Services
+{
+    public class BookService
+    {
+        public Book book { get; set; }
+
+        public void ReviewBook(string comment, double star, Book book)
+        {
+            this.book = book;
+
+            ParameterizedThreadStart staring = new ParameterizedThreadStart(CountStars);
+            ParameterizedThreadStart commenting = new ParameterizedThreadStart(AddComment);
+
+            Thread thread1 = new Thread(staring);
+            Thread thread2 = new Thread(commenting);
+
+            thread1.Start(star);
+            thread2.Start(comment);
+
+            thread1.Join();
+            thread2.Join();
+        }
+
+        public void CountStars(object star)
+        {
+            double stars = book.stars;
+            int reviewers = book.reviewers;
+
+            stars = stars * reviewers;
+            reviewers++;
+            stars = stars + Convert.ToInt32(star);
+            stars = stars / reviewers;
+
+            book.stars = stars ;
+            book.reviewers = reviewers;
+        }
+
+        public void AddComment(object comment)
+        {
+            book.comments.Add(comment.ToString());
+        }
+    }
+}
--- Virtual Librarian/VirtualLibrarian.Domain/Book.cs
+++ Virtual Librarian/VirtualLibrarian.Domain/Book.cs
@@ -61,33 +61,6 @@ namespace VirtualLibrarian.Domain
         {
             return this.ISBN;
         }
-        public void ReviewBook(string comment, double star)
-        {
-            ParameterizedThreadStart staring = new ParameterizedThreadStart(CountStars);
-            ParameterizedThreadStart commenting = new ParameterizedThreadStart(AddComment);
 
-            Thread thread1 = new Thread(staring);
-            Thread thread2 = new Thread(commenting);
-
-            thread1.Start(star);
-            thread2.Start(comment);
-
-            thread1.Join();
-            thread2.Join();
-
-        }
-
-        public void CountStars(object star)
-        {
-            stars = stars * reviewers;
-            reviewers++;
-            stars = stars + Convert.ToInt32(star);
-            stars = stars / reviewers;
-        }
-
-        public void AddComment(object comment)
-        {
-            comments.Add(comment.ToString());
-        }
     }
 }
